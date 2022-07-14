1f3ae8c1ef1837c5c0a9d85faafa706d90f85e1b
jrhe1213 <jiarong1213@gmail.com>
2022-07-13 22:34:11 -0400

    admin register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
+  constructor(private userService: UserService) {}
+
   @Post('admin/register')
-  register(@Body() body: RegisterDto) {
-    return body;
+  async register(@Body() body: RegisterDto) {
+    const { password_confirm, ...data } = body;
+    // valid check
+    if (data.password !== password_confirm) {
+      throw new BadRequestException('Password not match');
+    }
+    // save user
+    const hashed = await bcrypt.hash(data.password, 12);
+    return this.userService.save({
+      ...data,
+      password: hashed,
+      is_ambassador: false,
+    });
   }
 }
