b23c9cbf06a8aa6bdb41544f1dee484c81216d52
jrhe1213 <jiarong1213@gmail.com>
2022-07-13 23:06:54 -0400

    jwt & save it in cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,15 +4,21 @@ import {
   Controller,
   NotFoundException,
   Post,
+  Res,
 } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dtos/register.dto';
 import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post('admin/register')
   async register(@Body() body: RegisterDto) {
@@ -31,7 +37,13 @@ export class AuthController {
   }
 
   @Post('admin/login')
-  async login(@Body() body: LoginDto) {
+  async login(
+    @Body() body: LoginDto,
+    @Res({
+      passthrough: true, // save jwt into cookie
+    })
+    response: Response,
+  ) {
     const { email, password } = body;
     // find user by email
     const user = await this.userService.findOne({
@@ -44,7 +56,16 @@ export class AuthController {
     if (!(await bcrypt.compare(password, user.password))) {
       throw new BadRequestException('Invalid credential');
     }
-    // return user
-    return user;
+    // jwt token
+    const jwtToken = this.jwtService.signAsync({
+      id: user.id,
+    });
+    // set cookie
+    response.cookie('jwt', jwtToken, {
+      httpOnly: true,
+    });
+    return {
+      message: 'success',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // prefix all routes
   app.setGlobalPrefix('api');
+  // validate class
   app.useGlobalPipes(new ValidationPipe());
+  // cors
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:4300',
+      'http://localhost:5000',
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
