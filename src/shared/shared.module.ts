6e47b0a05f24f2199caaa9d0f72b0e5fac6152e8
jrhe1213 <jiarong1213@gmail.com>
2022-07-30 12:02:22 -0400

    feat: add redis cache manager

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    // jwt
     JwtModule.register({
       secret: 'secret',
       signOptions: {
         expiresIn: '1d',
       },
+    }),
+    // redis
+    CacheModule.register({
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
     }),
   ],
   exports: [JwtModule],
