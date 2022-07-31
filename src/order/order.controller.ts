0e7ccb47f29c761d33c2777cd2007fc323ba9109
jrhe1213 <jiarong1213@gmail.com>
2022-07-30 21:22:44 -0400

    feat: use transaction in order create - rollback if exception

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -20,6 +20,7 @@ import { Link } from 'src/link/link';
 import { ProductService } from 'src/product/product.service';
 import { OrderItem } from './order-item';
 import { Product } from 'src/product/product';
+import { DataSource } from 'typeorm';
 
 @Controller()
 export class OrderController {
@@ -28,6 +29,7 @@ export class OrderController {
     private orderItemService: OrderItemService,
     private linkService: LinkService,
     private productService: ProductService,
+    private dataSource: DataSource,
   ) {}
 
   @UseGuards(AuthGuard)
@@ -53,35 +55,47 @@ export class OrderController {
     if (!link) {
       throw new BadRequestException('Invalid link');
     }
-    // order
-    const o = new Order();
-    o.user_id = link.user.id;
-    o.ambassador_email = link.user.email;
-    o.first_name = body.first_name;
-    o.last_name = body.last_name;
-    o.email = body.email;
-    o.address = body.address;
-    o.country = body.country;
-    o.city = body.city;
-    o.zip = body.zip;
-    o.code = body.code;
-    const order = await this.orderService.save(o);
-    // products
-    for (const p of body.products) {
-      const product: Product = await this.productService.findOne({
-        id: p.product_id,
-      });
-      const orderItem = new OrderItem();
-      orderItem.order = order;
-      orderItem.product_title = product.title;
-      orderItem.price = product.price;
-      orderItem.quantity = p.quantity;
-      orderItem.ambassador_revenue = 0.1 * product.price * p.quantity;
-      orderItem.admin_revenue = 0.9 * product.price * p.quantity;
-      await this.orderItemService.save(orderItem);
+    // connection
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      // order
+      const o = new Order();
+      o.user_id = link.user.id;
+      o.ambassador_email = link.user.email;
+      o.first_name = body.first_name;
+      o.last_name = body.last_name;
+      o.email = body.email;
+      o.address = body.address;
+      o.country = body.country;
+      o.city = body.city;
+      o.zip = body.zip;
+      o.code = body.code;
+      const order = await queryRunner.manager.save(o);
+      // products
+      for (const p of body.products) {
+        const product: Product = await this.productService.findOne({
+          id: p.product_id,
+        });
+        const orderItem = new OrderItem();
+        orderItem.order = order;
+        orderItem.product_title = product.title;
+        orderItem.price = product.price;
+        orderItem.quantity = p.quantity;
+        orderItem.ambassador_revenue = 0.1 * product.price * p.quantity;
+        orderItem.admin_revenue = 0.9 * product.price * p.quantity;
+        await queryRunner.manager.save(orderItem);
+      }
+      //
+      await queryRunner.commitTransaction();
+      return order;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException();
+    } finally {
+      await queryRunner.release();
     }
-    //
-    return order;
   }
 
   @Get('admin/test_query_builder')
