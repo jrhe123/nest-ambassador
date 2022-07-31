4d56ab79f004e6a18fb8aa99aaa63a7a62417cf8
jrhe1213 <jiarong1213@gmail.com>
2022-07-30 21:04:51 -0400

    feat: create order api

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,10 @@
 import {
+  BadRequestException,
+  Body,
   ClassSerializerInterceptor,
   Controller,
   Get,
+  Post,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -10,12 +13,21 @@ import { faker } from '@faker-js/faker';
 import { randomInt } from 'crypto';
 import { OrderItemService } from './order-item.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateOrderDto } from './dtos/create-order.dto';
+import { LinkService } from 'src/link/link.service';
+import { Order } from './order';
+import { Link } from 'src/link/link';
+import { ProductService } from 'src/product/product.service';
+import { OrderItem } from './order-item';
+import { Product } from 'src/product/product';
 
 @Controller()
 export class OrderController {
   constructor(
     private orderService: OrderService,
     private orderItemService: OrderItemService,
+    private linkService: LinkService,
+    private productService: ProductService,
   ) {}
 
   @UseGuards(AuthGuard)
@@ -28,6 +40,48 @@ export class OrderController {
       },
       ['order_items'],
     );
+  }
+
+  @Post('checkout/orders')
+  async create(@Body() body: CreateOrderDto) {
+    const link: Link = await this.linkService.findOne(
+      {
+        code: body.code,
+      },
+      ['user'],
+    );
+    if (!link) {
+      throw new BadRequestException('Invalid link');
+    }
+    // order
+    const o = new Order();
+    o.user_id = link.user.id;
+    o.ambassador_email = link.user.email;
+    o.first_name = body.first_name;
+    o.last_name = body.last_name;
+    o.email = body.email;
+    o.address = body.address;
+    o.country = body.country;
+    o.city = body.city;
+    o.zip = body.zip;
+    o.code = body.code;
+    const order = await this.orderService.save(o);
+    // products
+    for (const p of body.products) {
+      const product: Product = await this.productService.findOne({
+        id: p.product_id,
+      });
+      const orderItem = new OrderItem();
+      orderItem.order = order;
+      orderItem.product_title = product.title;
+      orderItem.price = product.price;
+      orderItem.quantity = p.quantity;
+      orderItem.ambassador_revenue = 0.1 * product.price * p.quantity;
+      orderItem.admin_revenue = 0.9 * product.price * p.quantity;
+      await this.orderItemService.save(orderItem);
+    }
+    //
+    return order;
   }
 
   @Get('admin/test_query_builder')
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LinkModule } from 'src/link/link.module';
+import { ProductModule } from 'src/product/product.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { Order } from './order';
 import { OrderItem } from './order-item';
@@ -8,7 +10,12 @@ import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem]), SharedModule],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem]),
+    SharedModule,
+    LinkModule,
+    ProductModule,
+  ],
   controllers: [OrderController],
   providers: [OrderService, OrderItemService],
   exports: [OrderService, OrderItemService],
